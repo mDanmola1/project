095d6afee4709cf9faeb6af287270afa3428d6be
mDanmola1 <157433329+mDanmola1@users.noreply.github.com>
2024-05-15 17:05:36 -0400

    Delete anchorgames24 directory

--- anchorgames24/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-// Set strictQuery to true to suppress the warning
-mongoose.set('strictQuery', true);
-
-mongoose.connect('mongodb://127.0.0.1:27017/anchor-games', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
